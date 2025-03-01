5a7862c5d51ce393a70e3cf9b93681eac1f57c6c
Roarke De Crewe <r.decrewe@hotmail.ca>
2025-03-01 05:34:29 -0800

    Completed main page and added files for all others

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.filter = "brightness(25%)";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.filter = "brightness(100%)";
+  } 
