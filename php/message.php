1be8c6e83dbf496aad03fd8164a017070f0c3e2c
Roarke De Crewe <r.decrewe@hotmail.ca>
2025-03-23 19:40:57 -0700

    REALLY made it safe now

--- php/message.php
+++ php/message.php
@@ -2,9 +2,9 @@
 require_once('sessionstart.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_SESSION['username'])) {
-        $susername = mysqli_real_escape_string($_SESSION['username']);
-        $rusername = mysqli_real_escape_string($_POST['rusername']);
-        $message = mysqli_real_escape_string($_POST['message']);
+        $susername = mysqli_real_escape_string($connection,$_SESSION['username']);
+        $rusername = mysqli_real_escape_string($connection,$_POST['rusername']);
+        $message = mysqli_real_escape_string($connection,$_POST['message']);
         date_default_timezone_set('America/Los_Angeles');
         $date = date('Y-m-d\TG:i');
         $sql = "START TRANSACTION";
