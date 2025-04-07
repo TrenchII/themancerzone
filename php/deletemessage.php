1b703557daec737b2a9929a44d462b88ad469588
TrenchII <r.decrewe@hotmail.ca>
2025-04-07 00:21:03 -0700

    Update deletemessage.php

--- php/deletemessage.php
+++ php/deletemessage.php
@@ -1,1 +1,22 @@
+<?php
+require_once('connectDB.php');
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if(isset($_GET['messageid'])) {
+        $lessonid = $_GET['messageid'];
+        $sql = "START TRANSACTION";
+        mysqli_query($connection,$sql);
 
+        $sql = "DELETE FROM message WHERE messageid='$messageid'";
+        mysqli_query($connection,$sql);
+
+        $sql = "COMMIT";
+        mysqli_query($connection,$sql);
+
+        header('location:/rdecrewe/themancerzone/mainpage.php');
+        mysqli_close($connection);
+        die();
+    }
+}
+header('location:'.$_SERVER['HTTP_REFERER']);
+mysqli_close($connection);
+?>
